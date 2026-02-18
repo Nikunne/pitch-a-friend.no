100aff77b3815e2f389f3de6fd68f23c004b372d
Niklas Unneland <niklasu@Niklas-sin-MacBook-Air.local>
2026-02-18 16:32:22 +0100

    added link-button style

--- components/PatentPage.jsx
+++ components/PatentPage.jsx
@@ -4,7 +4,7 @@ function PatentPage(props) {
             <h2>{props.trademark}</h2>
             <p> {props.trademark + " er ikke et registrert varemerke i Norge"}</p>
             <img className="patent-img" src={"/"+props.img} alt="Patent logo" />
-            <a target={"_blank"} className={"linken"} href={props.url}> <img id="mini" src={"/Patentstyret-logo.png"}/> Source</a>
+            <a target={"_blank"} className={"linken"} href={props.url}> <img className="mini" src={"/Patentstyret-logo.png"}/> <p className={"mini-text"}>Source</p></a>
         </>
     )
 }
