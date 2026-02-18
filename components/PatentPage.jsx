2da567454e9ef47c642484ce8827103f5374cf01
Niklas Unneland <niklasu@dhcp-10-22-121-56.wlan.ntnu.no>
2026-02-18 14:16:25 +0100

    minor h3 to h2

--- components/PatentPage.jsx
+++ components/PatentPage.jsx
@@ -1,7 +1,7 @@
 function PatentPage(props) {
     return (
         <>
-            <h3>{props.trademark}</h3>
+            <h2>{props.trademark}</h2>
             <p> {props.trademark + " er ikke et registrert varemerke i Norge"}</p>
             <img className="patent-img" src={"/"+props.img} alt="Patent logo" />
             <a href={props.url}> Patentstyret-Link</a>
